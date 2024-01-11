d79d70cc22a14904cc67b3aef6832620e2c77838
osterburg <timo.osterburg@tu-dortmund.de>
2024-01-11 12:20:35 +0100

    added function to add resolvers instead on directly doing it on import

--- src/omega_conf_resolver/__init__.py
+++ src/omega_conf_resolver/__init__.py
@@ -4,14 +4,15 @@ import importlib
 from omegaconf import OmegaConf
 from .omega_conf_resolver import OmegaConfResolver
 
-for name, obj in inspect.getmembers(
-    sys.modules["omega_conf_resolver.omega_conf_resolver"]
-):
-    if (
-        inspect.isclass(obj)
-        and issubclass(obj, OmegaConfResolver)
-        and not inspect.isabstract(obj)
+def add_costum_resolvers():
+    for name, obj in inspect.getmembers(
+        sys.modules["omega_conf_resolver.omega_conf_resolver"]
     ):
-        OmegaConf.register_new_resolver(name, obj())
+        if (
+            inspect.isclass(obj)
+            and issubclass(obj, OmegaConfResolver)
+            and not inspect.isabstract(obj)
+        ):
+            OmegaConf.register_new_resolver(name, obj())
 
-        # importlib.import_module(name, package="omega_conf_resolver.omega_conf_resolver")
+            # importlib.import_module(name, package="omega_conf_resolver.omega_conf_resolver")
